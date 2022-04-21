7d733062b02064b75219dc428278fe1381aa840d
mattjennings <me@mattjennings.io>
2022-04-21 16:29:25 -0500

    add custom pixi.js

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,21 @@
 import adapter from '@sveltejs/adapter-auto'
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
   kit: {
-    adapter: adapter(),
+    adapter: adapter(),    
     vite: {
       optimizeDeps: {
-        include: ['pixi.js'],
+        exclude: ['svelte-pixi']
+      },      
+      resolve: {
+        alias: {
+          'pixi.js': path.resolve(__dirname, 'src/lib/pixi.js'),
+        },
       },
     },
   },
