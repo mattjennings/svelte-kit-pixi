a74bcf3bb9d93877ddf98b5512ff8525bc4669f6
mattjennings <me@mattjennings.io>
2022-04-21 16:31:36 -0500

    fix ssr error in build

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,3 +1,7 @@
+<script>
+  import { browser } from "$app/env";
+</script>
+
 <!-- 
   This ensures pixi and its dependencies are only loaded on the client, as
   pixi.js cannot be imported server-side without errors
@@ -5,6 +9,8 @@
   Alternatively, you could disable SSR for the route(s) via https://kit.svelte.dev/docs/hooks#handle
   which would allow you to import Pixi components as normal.
 -->
-{#await import('$lib/PixiApp.svelte') then app}
-  <svelte:component this={app.default} />
-{/await}
+{#if browser}
+  {#await import("$lib/PixiApp.svelte") then app}
+    <svelte:component this={app.default} />
+  {/await}
+{/if}
